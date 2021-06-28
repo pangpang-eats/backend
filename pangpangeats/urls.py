6d9efb02a99fe927a03bafb76d1cf56fd1e364b8
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-28 23:17:58 +0900

    feat: api 문서 관련 라우팅 추가

--- pangpangeats/urls.py
+++ pangpangeats/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(),
+         name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(), name='redoc'),
 ]
