b865ae7d552da00d2fad7d228facaedf441635c7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-02 00:54:08 +0900

    feat: api 라우팅을 위한 urls.py 추가

--- pangpangeats/urls.py
+++ pangpangeats/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from api import urls as api_urls
 
 urlpatterns = [
+    path('api/', include(api_urls)),
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/',
