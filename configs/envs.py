9ff93348326ad39e2a2f14b8df8dbdca60ff9bd7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-02 22:34:25 +0900

    chore: psql db 관련 설정 추가

--- configs/envs.py
+++ configs/envs.py
@@ -7,3 +7,9 @@ SECRET_KEY = environ.get("SECRET_KEY",
                          'b9-ct%3wayay6md-9gi=w-r$c^-7ior6@i2*@m^nh-x=yz9xxz')
 
 DEBUG = environ.get("DEBUG", "true") == "true"
+
+DB_HOST = environ.get("POSTGRES_HOST", "localhost")
+DB_PORT = environ.get("POSTGRES_PORT", "5432")
+DB_NAME = environ.get("POSTGRES_DB", "postgres")
+DB_USER = environ.get("POSTGRES_USER", "postgres")
+DB_PASSWORD = environ.get("POSTGRES_PASSWORD", "postgres")
--- pangpangeats/settings.py
+++ pangpangeats/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'pangpangeats.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': envs.DB_HOST,
+        'PORT': envs.DB_PORT,
+        'NAME': envs.DB_NAME,
+        'USER': envs.DB_USER,
+        'PASSWORD': envs.DB_PASSWORD,
     }
 }
 
