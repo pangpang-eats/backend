75fb755eaa88d4fc698ab19781b77b4e9201c26f
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-03 20:39:58 +0900

    test(user): CreditCard 모델 테스트 추가

--- apps/user/tests/test_models.py
+++ apps/user/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from django.db.utils import DataError
+from django.db.utils import DataError, IntegrityError
 from django.core.exceptions import ValidationError
-from apps.user.models import UserRole, User
+from apps.user.models import UserRole, User, CreditCard
 
 
 class UserCreationTest(TestCase):
@@ -59,3 +59,69 @@ class UserCreationTest(TestCase):
     def test_create_superuser_should_success(self):
         User.objects.create_superuser(phone_number='01012341234',
                                       password='thePas123Q')
+
+
+class CreditCardTest(TestCase):
+    def test_credit_card_creation_with_new_user_should_success(self):
+        user = User.objects.create_user(phone_number='01012341234',
+                                        nickname='mengmota',
+                                        password='thePas123Q',
+                                        role=UserRole.CLIENT)
+        credit_card = CreditCard.objects.create(owner=user,
+                                                alias='my awesome card',
+                                                card_number='1234567890123456',
+                                                cvc='123',
+                                                expiry_year=2032,
+                                                expiry_month=12)
+        self.assertEqual(credit_card.owner, user)
+        self.assertEqual(credit_card.alias, 'my awesome card')
+        self.assertEqual(credit_card.card_number, '1234567890123456')
+        self.assertEqual(credit_card.cvc, '123')
+        self.assertEqual(credit_card.expiry_year, 2032)
+        self.assertEqual(credit_card.expiry_month, 12)
+        self.assertEqual(str(credit_card), "1234-****-****-****")
+
+    def test_credit_card_creation_with_wrong_expiry_should_fail(self):
+        user = User.objects.create_user(phone_number='01012341234',
+                                        nickname='mengmota',
+                                        password='thePas123Q',
+                                        role=UserRole.CLIENT)
+        with self.assertRaises(ValidationError):
+            credit_card = CreditCard(owner=user,
+                                     alias='my awesome card',
+                                     card_number='1234567890123456',
+                                     cvc='123',
+                                     expiry_year=None,
+                                     expiry_month=None)
+            credit_card.full_clean()
+            credit_card.save()
+
+    def test_credit_card_creation_with_wrong_cvc_should_fail(self):
+        user = User.objects.create_user(phone_number='01012341234',
+                                        nickname='mengmota',
+                                        password='thePas123Q',
+                                        role=UserRole.CLIENT)
+        with self.assertRaises(IntegrityError):
+            credit_card = CreditCard(owner=user,
+                                     alias='my awesome card',
+                                     card_number='1234567890123456',
+                                     cvc=None,
+                                     expiry_year=2032,
+                                     expiry_month=12)
+            credit_card.save()
+
+    def test_credit_card_creation_with_wrong_card_number_should_fail(self):
+        """all of the codes are the same as the upper test, but the length of card_number should be 1."""
+        user = User.objects.create_user(phone_number='01012341234',
+                                        nickname='mengmota',
+                                        password='thePas123Q',
+                                        role=UserRole.CLIENT)
+        with self.assertRaises(ValidationError):
+            credit_card = CreditCard(owner=user,
+                                     alias='my awesome card',
+                                     card_number='1',
+                                     cvc='123',
+                                     expiry_year=2032,
+                                     expiry_month=12)
+            credit_card.full_clean()
+            credit_card.save()
