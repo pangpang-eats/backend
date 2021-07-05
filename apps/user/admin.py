df968b79b5d958ab13c2434b5996b50794bdae16
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-05 15:12:52 +0900

    feat(user): UserAdmin에 CreditCardInline 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 
 
+class CreditCardInline(admin.StackedInline):
+    model = models.CreditCard
+
+
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('phone_number', 'nickname', 'role', 'date_joined')
+    list_display = ('phone_number', 'name', 'role', 'date_joined')
+    inlines = (CreditCardInline, )
