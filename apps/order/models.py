0061b47cdcd90a9faac2c72f94b99680c8444bd7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-06 13:01:32 +0900

    refactor(credit_card): credit card 앱 분리

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,7 +1,8 @@
 import typing
 from django.db import models
 from pangpangeats.settings import AUTH_USER_MODEL
-from apps.user.models import CreditCard, User
+from apps.user.models import User
+from apps.credit_card.models import CreditCard
 from apps.restaurant.models import MenuInformation
 
 
