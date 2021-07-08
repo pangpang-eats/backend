ebf77427b6606e081131243cff5ac43a4593c4b6
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-08 16:11:56 +0900

    feat(user): 여러 기능을 담은 UserView 추가
    - 요청자 프로필 반환하는 기능
    - 회원가입 기능

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http.request import HttpRequest
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.user.models import User
+from apps.user.serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'register':
+            return (permissions.AllowAny(), )
+        return (permissions.IsAuthenticated(), )
+
+    def list(self, request: HttpRequest):  # retrieve requested user's profile
+        user: User = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=False)
+    def register(self, request: HttpRequest, *args, **kwargs):
+        serializer: UserSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
