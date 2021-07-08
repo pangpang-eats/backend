cb226c64ee103d453bc87b48cd379f53c93ece86
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-08 16:12:17 +0900

    feat(api): 사용자 및 인증 관련 라우트 추가

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,15 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from apps.user.views import UserView
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'users', UserView, basename='users')
+
+urlpatterns = [
+    path('token', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
+    path('token/verify', TokenVerifyView.as_view()),
+]
+
+urlpatterns += router.urls
