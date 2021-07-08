171ea77cb384a8ef5aa2be0f2890751f19f8f6b2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-08 16:10:35 +0900

    fix(user): User 모델에서 필요없는 모델 삭제

--- apps/user/models.py
+++ apps/user/models.py
@@ -73,7 +73,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
 
     date_joined = models.DateTimeField(auto_now_add=True)
 
