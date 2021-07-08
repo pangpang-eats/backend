3d4a997495ee861832780bc099275f6f5f792b30
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-08 15:50:50 +0900

    chore: settings.py 에 simplejwt 관련 기능 추가

--- pangpangeats/settings.py
+++ pangpangeats/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # installed modules
     'rest_framework',
+    'rest_framework_simplejwt',
     'drf_spectacular',
     'model_utils',
     'safedelete',
@@ -141,7 +142,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS':
     'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES':
-    ('rest_framework.authentication.TokenAuthentication', )
+    ('rest_framework_simplejwt.authentication.JWTAuthentication', )
 }
 
 SPECTACULAR_SETTINGS = {
