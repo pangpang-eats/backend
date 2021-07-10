d3626b21c6572662e7e0781d219c05ef191b6e7e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-10 13:53:39 +0900

    test(credit_card): TestCreditCardView 테스트 스펙 추가

--- apps/credit_card/tests/test_views.py
+++ apps/credit_card/tests/test_views.py
@@ -18,3 +18,22 @@ class TestCreditCardAssignmentView(APITestCase):
     5. the expiry_month should be between 1 and 12
     6. the expiry_year should be bigger than the current year
     """
+class TestCreditCardView(APITestCase):
+    ENDPOINT = '/api/credit-cards'
+    user1: User
+    user2: User
+    user1_token: str
+    user2_token: str
+    """
+    Test the API self.ENDPOINT to features for credit card
+
+    the self.ENDPOINT should work like:
+    1. it should return 401 when accessing to a specific card (like get, delete)
+       without credentials or if the requested user is not the owner
+    2. three api self.ENDPOINTs should work in total with credentials, and they are:
+        2-1. list view
+        2-2. detail view (get) - 404 if the there are no card with the given pk
+        2-3. update and partial_update view (put, patch) - 404 if there are no card with the given pk
+        2-3.1. but the 'alias' field only can be editted
+        2-4. delete view - 404 if there are no card with the given pk
+    """
