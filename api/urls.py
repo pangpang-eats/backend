82b3dc2765d91d658fc44e014637924ddeb9d5fb
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-13 13:58:49 +0900

    feat: urls.py 에 credit_card 추가

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from apps.user.views import UserView
+from apps.credit_card.views import CreditCardView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'users', UserView, basename='users')
+router.register(r'credit-cards', CreditCardView, basename='credit_cards')
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view()),
