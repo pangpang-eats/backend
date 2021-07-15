99e06d7f7f1630bfecddd0077423e305e9776579
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-15 17:41:04 +0900

    feat(restaurant): admin 페이지에 RestaurantAdmin 등록

--- apps/restaurant/admin.py
+++ apps/restaurant/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from apps.restaurant.models import BusinessInformation, Location, MenuInformation, Restaurant
 
-# Register your models here.
+
+class LocationInline(admin.TabularInline):
+    model = Location
+
+
+class BusinessInformationInline(admin.TabularInline):
+    model = BusinessInformation
+
+
+class MenuInformationInline(admin.StackedInline):
+    model = MenuInformation
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'picture', 'minimum_order_cost',
+                    'minimum_delivery_cost', 'telephone_number', 'description',
+                    'notice', 'origin_information', 'nuturition_facts',
+                    'allergens_facts')
+    inlines = (LocationInline, BusinessInformationInline,
+               MenuInformationInline)
