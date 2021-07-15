ec0add66ced437cf7f5c5dc582ad81dd92cbadd2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-15 17:41:37 +0900

    feat(user): admin 페이지에 RestaurantNestedInline 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from nested_inline.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
+from apps.restaurant.models import BusinessInformation, Location, MenuInformation, Restaurant
 from apps.credit_card.models import CreditCard
 from apps.user.models import User
+
+
+class RestaurantNestedInline(NestedStackedInline):
+    model = Restaurant
+
+    class LocationNestedInline(NestedTabularInline):
+        model = Location
+        fk_name = 'restaurant'
+        extra = 1
+
+    class BusinessInformationNestedInline(NestedTabularInline):
+        model = BusinessInformation
+        fk_name = 'restaurant'
+        extra = 1
+
+    class MenuInformationNestedInline(NestedStackedInline):
+        model = MenuInformation
+        fk_name = 'restaurant'
+        extra = 1
+
+    inlines = [
+        LocationNestedInline, BusinessInformationNestedInline,
+        MenuInformationNestedInline
+    ]
 
 
 class CreditCardInline(admin.StackedInline):
@@ -8,6 +34,6 @@ class CreditCardInline(admin.StackedInline):
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(NestedModelAdmin):
     list_display = ('phone_number', 'name', 'role', 'date_joined')
-    inlines = (CreditCardInline, )
+    inlines = (CreditCardInline, RestaurantNestedInline)
