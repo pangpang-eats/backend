081bf991879c58fd7ba8dab6cce81219a2d3bb1d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-20 20:10:14 +0900

    fix(user): #15

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from apps.user.models import User, UserRole
@@ -9,6 +10,11 @@ class UserSerializer(serializers.ModelSerializer):
     role = serializers.ChoiceField(read_only=True, choices=UserRole.choices)
     is_verified = serializers.BooleanField(read_only=True)
     date_joined = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data: Dict):
+        user = User.objects.create_user(**validated_data)
+        user.save()
+        return user
 
     class Meta:
         model = User
