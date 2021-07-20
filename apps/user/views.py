952058182054492872d97a1393c0a8746496489b
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-20 22:27:27 +0900

    feat(user): /api/users/profile 라우트 추가
    - GET 요청시 프로필 조회 (오버라이드 된 list 가 하던 역할 대체)
    - PATCH 요청시 프로필 업데이트 (Fix #17)

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from django.http.request import HttpRequest
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
@@ -18,6 +19,24 @@ class UserView(viewsets.GenericViewSet):
         if self.action == 'register':
             return UserRegisterSerializer
         return UserSerializer
+
+    def update_profile(self, user, data) -> Dict:
+        serializer: UserSerializer = UserSerializer(user,
+                                                    data=data,
+                                                    partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return serializer.data
+
+    @action(methods=['get', 'patch'], detail=False)
+    def profile(self, request: HttpRequest):
+        user: User = request.user
+        if request.method == 'GET':  # return user's profile
+            serializer: UserSerializer = self.get_serializer(user)
+            return Response(serializer.data)
+        elif request.method == 'PATCH':  # update and return one's updated user profile
+            updated_data = self.update_profile(user, request.data)
+            return Response(updated_data)
 
     @action(methods=['post'], detail=False)
     def register(self, request: HttpRequest, *args, **kwargs):
